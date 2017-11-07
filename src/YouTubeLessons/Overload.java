1de2c88d92128af54a08c7a65d29de90082ffea8
user <mifotosave@gmail.com>
2017-11-07 20:59:58 +0200

    The first project

--- src/YouTubeLessons/Overload.java
+++ src/YouTubeLessons/Overload.java
@@ -1,4 +1,22 @@
 package YouTubeLessons;
 
 public class Overload {
+    public static int addTwoDigits(int a, int b) throws Exception{
+        return a + b;
+    }
+    protected final double addTwoDigits() throws RuntimeException{
+        return 4;
+    }
+
+    public static void main(String[] args) throws Exception {
+        OverloadChild overload = new OverloadChild();
+        System.out.println(overload.addTwoDigits(5, 6));
+        System.out.println(overload.addTwoDigits());
+        overload.addTwoDigits(5);
+    }
+}
+class OverloadChild extends Overload{
+    void addTwoDigits(int i){
+        System.out.println("overload method");
+    }
 }
