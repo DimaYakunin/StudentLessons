4a8b31265c884c3b14a48b190f319d0c182b74f4
user <mifotosave@gmail.com>
2017-11-07 22:08:31 +0200

    The first project

--- src/YouTubeLessons/Overriding.java
+++ src/YouTubeLessons/Overriding.java
@@ -1,4 +1,27 @@
 package YouTubeLessons;
 
+/*
+перегрузка статических методов не осуществляеться
+ */
+class Parent {
+    Parent method(){
+        return new Parent();
+    }
+}
+
+class Child extends Parent{
+    @Override
+    Child method(){
+        return new Child();
+    }
+
+}
 public class Overriding {
+    public static void main(String[] args) {
+        Parent parent = new Parent();
+        Parent child = new Child();
+        System.out.println(parent.method());
+        System.out.println(child.method());
+    }
+
 }
