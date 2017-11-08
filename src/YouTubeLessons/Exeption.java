3efe98f9c8a1b8f7e12311c4372cd5718373ee93
user <mifotosave@gmail.com>
2017-11-08 14:09:22 +0200

    Exeption lesson

---
+++ src/YouTubeLessons/Exeption.java
@@ -0,0 +1,27 @@
+package YouTubeLessons;
+
+public class Exeption {
+    public static void main(String[] args) {
+        new Exeption().someMethod();
+    }
+
+    void someMethod() {
+        try {
+            System.out.println("try");
+            Object o = null;
+            o.hashCode();
+            //throw new Exception();
+        } catch (NullPointerException e) {
+            System.out.println("catch");
+            e.printStackTrace();
+            /*
+            можно отправить себе стак ошибок себе на почту
+            и исправить баги программы и исправить их в следующей
+            версии
+             */
+
+        }finally {
+            System.out.println("finally");
+        }
+    }
+}
